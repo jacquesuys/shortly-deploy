26672996e6e442a926008a8a87146930f9ecdc89
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 19:54:21 -0800

    Conversion to MongoDB

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,31 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
+var userSchema = mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+  timestamps: Date
+});
+
+var User = mongoose.model('User',userSchema);
+
+User.prototype.comparePassword = function(attemptedPassword, callback){
+  bcrypt.compare(attemptedPassword, this.password,
+  function(err, isMatch) {
+    if (err) { callback(err); }
+    callback(isMatch);
+  });
+};
+
+userSchema.pre('save', function(next){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
     });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
 });
 
 module.exports = User;
